924a1aa13b483facb3d51786e9a977d94ff18cdc
AMANVerma28 <amansingh.v15@iiits.in>
2017-11-01 15:53:38 +0530

    gdal added

--- itsserver/settings.py
+++ itsserver/settings.py
@@ -15,7 +15,9 @@ import dj_database_url
 
 db_from_env = dj_database_url.config()
 DATABASES = { 'default': dj_database_url.config() }
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = "{}/libgeos_c.so".format(environ.get('GEOS_LIBRARY_PATH'))
+GDAL_LIBRARY_PATH = "{}/libgdal.so".format(environ.get('GDAL_LIBRARY_PATH'))
+PROJ4_LIBRARY_PATH = "{}/libproj.so".format(environ.get('PROJ4_LIBRARY_PATH'))
 #DATABASES['default'].update(db_from_env)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
